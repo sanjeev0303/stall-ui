2e2abc104ffeae290d8b4abeb33f08eea84e4a8f
shivajee98 <ultimateintimater@gmail.com>
2025-02-24 12:30:03 +0530

    required image

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Stall from './_components/Stall'
+const RootPage = () => {
+  return (
+    <div className='relative bg-yellow-400 w-screen h-dvh flex justify-center items-center'>
+      <Stall />
+    </div>
+  )
+}
+
+export default RootPage
