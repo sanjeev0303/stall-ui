a0d9c88cb1d539dc9c0033beea07a17d833752b2
sanjeev0303 <sanjeev.081021@gmail.com>
2025-03-18 11:55:00 +0530

    Replace Stall component with StallDemo in RootPage

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Stall from './_components/Stall'
+import StallDemo from './_components/StallDemo'
 const RootPage = () => {
   return (
     <div className=' bg-yellow-400  text-white w-full h-screen overflow-y-hidden'>
-      <Stall />
+      <StallDemo />
     </div>
   )
 }
